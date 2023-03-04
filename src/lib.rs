691b38dd4ed68d6957e46ab9376f7047dea71e82
lauglam <lauglam@outlook.com>
2023-03-04 23:01:46 +0800

    feat(parse file): add impl from serde-yaml error for parse error

--- src/lib.rs
+++ src/lib.rs
@@ -99,11 +99,18 @@ pub type ParseResult<T> = Result<T, ParseError>;
 
 pub enum ParseError {
     MissingProperties,
+    ParseParseFileFail(serde_yaml::Error),
     ActionError(ActionError),
 }
 
 impl From<ActionError> for ParseError {
     fn from(value: ActionError) -> Self {
         ParseError::ActionError(value)
+    }
+}
+
+impl From<serde_yaml::Error> for ParseError {
+    fn from(value: serde_yaml::Error) -> Self {
+        ParseError::ParseParseFileFail(value)
     }
 }
